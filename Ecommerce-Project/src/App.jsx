09ae685e5694d20db8aeb0a2999fa88076b4be4b
aayushjat688 <aayushjat688@gmail.com>
2026-05-28 13:55:31 +0530

    Create multiple pages using eact-router

--- Ecommerce-Project/src/App.jsx
+++ Ecommerce-Project/src/App.jsx
@@ -1,6 +1,12 @@
+import { Routes,Route } from 'react-router-dom'
+
 import { HomePage } from "./pages/HomePage";
 export function App () {
   return (
-    <HomePage />
+    <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='checkout' element={<div>Test Checkout</div>} />
+    </Routes>
+    
   );
 }

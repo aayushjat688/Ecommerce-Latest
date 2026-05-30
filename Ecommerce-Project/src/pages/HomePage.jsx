198f15382eac59a44c0ef447efe4e18bb957427f
aayushjat688 <aayushjat688@gmail.com>
2026-05-30 13:15:50 +0530

    Get cart from the backend and display quantity

--- Ecommerce-Project/src/pages/HomePage.jsx
+++ Ecommerce-Project/src/pages/HomePage.jsx
@@ -6,12 +6,18 @@ import { Header } from '../components/Header';
 import './HomePage.css'
 export function HomePage () {
   const [products , setProducts] = useState([]);
+  const [cart , setCart] = useState([]);
 
   useEffect(()=>{
 
     axios.get('http://localhost:3000/api/products')
   .then((response)=>{
     setProducts(response.data);
+});
+
+axios.get('http://localhost:3000/api/cart-items')
+.then((response)=>{
+    setCart(response.data);
 })
   },[]);
   
@@ -20,7 +26,7 @@ export function HomePage () {
   <>
   <title>Ecommerce Project</title>
            <link rel="icon" type="image/svg+xml" href="home-favicon.png" />
-       <Header />
+       <Header cart={cart}/>
 
         <div className="home-page">
           <div className="products-grid">
@@ -40,8 +46,9 @@ export function HomePage () {
               </div>
 
               <div className="product-rating-container">
+                
                 <img className="product-rating-stars"
-                  src={product.rating.stars} />
+                  src={`images/ratings/rating-${product.rating.stars*10}.png`} />
                 <div className="product-rating-count link-primary">
                   {product.rating.count}
                 </div>
